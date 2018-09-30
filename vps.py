29f0ff0615b352e580527daacbd6be2d8019831c
Ben Joyner <bjoyner12690@protonmail.com>
2018-09-30 04:47:14 -0400

    Added color

--- vps.py
+++ vps.py
@@ -5,6 +5,10 @@ import argparse
 
 #TODO Add proper error messages
 
+class colors():
+    green = str('\033[32m')
+    default = str('\033[0m')
+
 class parseArguments():
     parser = argparse.ArgumentParser(description='A simple Xbps wrapper') 
     parser.add_argument('operation', help='Usage: update, remove, install, search')
@@ -12,19 +16,19 @@ class parseArguments():
     args = parser.parse_args()
 
 def update():
-    print('Running sudo xbps-install -Su')
+    print(c.green + 'Running sudo xbps-install -Su' + c.default)
     os.system('sudo xbps-install -Su')
 
 def remove():
-    print('Running sudo xbps-remove ' + p.args.package)
+    print(c.green + 'Running sudo xbps-remove ' + p.args.package + c.default)
     os.system('sudo xbps-remove ' + p.args.package)
 
 def install():
-    print('Running sudo xbps-install ' + p.args.package)
+    print(c.green + 'Running sudo xbps-install ' + p.args.package + c.default)
     os.system('sudo xbps-install ' + p.args.package)
 
 def search():
-    print('Running xbps-query -Rs ' + p.args.package)
+    print(c.green + 'Running xbps-query -Rs ' + p.args.package + c.default)
     os.system('xbps-query -Rs ' + p.args.package)
 
 def main():
@@ -39,7 +43,8 @@ def main():
     else:
         p.parser.print_help()
 
-p=parseArguments()
+p = parseArguments()
+c = colors()
 
 if __name__ =='__main__':
 	main()
